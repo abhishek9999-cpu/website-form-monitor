e45514bc9dced01af88ae8f88f94ba62cd9bb915
abhishek9999-cpu <asntorento9999@gmail.com>
2026-07-22 15:23:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require("axios");
 const nodemailer = require("nodemailer");
 
-const websiteURL = "https://www.dbuzzz.com/thank-you-page";
+const websiteURL = "https://www.dbuzzz.com";
 
 async function checkWebsite() {
   try {
